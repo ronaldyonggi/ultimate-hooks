8ec6762df1c3c4bace8608cfd784218405471b69
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-13 16:42:42 +0800

    7.8 created useResource custom hook

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import axios from "axios"
 
 export const useField = type => {
     const [value, setValue] = useState('')
@@ -6,4 +7,34 @@ export const useField = type => {
     const onChange = e => setValue(e.target.value)
 
     return { type, value, onChange}
+}
+
+export const useResource = baseUrl => {
+
+    let token = null
+
+    const setToken = newToken => {
+        token = `bearer ${newToken}`
+    }
+
+    const getAll = async () => {
+        const res = await axios.get(baseUrl)
+        return res.data
+    }
+
+    const create = async newObject => {
+        const config = {
+            headers: { Authorization: token }
+        }
+
+        const res = await axios.post(baseUrl, newObject, config)
+        return res.data
+    }
+
+    const update = async (id, newObject) => {
+        const res = await axios.put(`${ baseUrl }/${id}`, newObject)
+        return res.data
+    }
+
+    return { getAll, create, update, setToken}
 }
