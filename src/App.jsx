592de7677bec187aa1b6b7f8b5ddb61e8d70853f
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-13 16:57:34 +0800

    7.8 App implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
-import { useState } from 'react'
+import { useField, useResource } from "./hooks"
 
-function App() {
+const App = () => {
+  const content = useField('text')
+  const name = useField('text')
+  const number = useField('text')
+
+  const [notes, noteService] = useResource('http://localhost:3005/notes')
+  const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  const handleNoteSubmit = e => {
+    e.preventDefault()
+    noteService.create({ content: content.value })
+  }
+
+  const handlePersonSubmit = e => {
+    e.preventDefault()
+    personService.create({ name: name.value, number: number.value })
+  }
 
   return (
+    <div>
+      <h2>Notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content}/>
+        <button>create</button>
+      </form>
+      {notes.map(n => (
+        <p key={n.id}>{n.content}</p>
+      ))}
+
+      <h2>Persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name}/> <br />
+        number <input {...number}/>
+        <button>create</button>
+      </form>
+      {persons.map(p => (
+        <p key={p.id}>{p.name} {p.number}</p>
+      ))}
+    </div>
   )
 }
 
