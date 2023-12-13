16509e39cdd4d8d3a7f33077624772c33bcb3846
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-13 17:14:00 +0800

    7.8 useResource hook modifies frontend components

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 export const useField = type => {
@@ -10,6 +10,7 @@ export const useField = type => {
 }
 
 export const useResource = baseUrl => {
+    const [resources, setResources] = useState([])
 
     let token = null
 
@@ -19,8 +20,18 @@ export const useResource = baseUrl => {
 
     const getAll = async () => {
         const res = await axios.get(baseUrl)
+        setResources(res.data)
         return res.data
     }
+
+    useEffect(() => {
+        const fetchAllResources = async () => {
+            const fetchedResources = await getAll()
+            setResources(fetchedResources)
+        }
+
+        fetchAllResources()
+    }, [])
 
     const create = async newObject => {
         const config = {
@@ -28,13 +39,15 @@ export const useResource = baseUrl => {
         }
 
         const res = await axios.post(baseUrl, newObject, config)
+        setResources(resources.concat(res.data))
         return res.data
     }
 
     const update = async (id, newObject) => {
         const res = await axios.put(`${ baseUrl }/${id}`, newObject)
+        setResources(resources.map(r => r.id === id ? res.data : r))
         return res.data
     }
 
-    return { getAll, create, update, setToken}
+    return [resources, { getAll, create, update, setToken}]
 }
